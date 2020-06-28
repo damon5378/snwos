a02678cdbb9d7a7f41db32a45c63adc9db9716bc
damon5378 <iosifov.konstantin@gmail.com>
2020-06-28 18:27:39 +0300

    Updates

---
+++ precache-manifest.cf81463e30c622ca1e535deb2a2c8a39.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "148de5037d3344b3fd596ba4f0ab7778",
+    "url": "/snwos/index.html"
+  },
+  {
+    "revision": "12b15e30905886c1867b",
+    "url": "/snwos/static/css/3.e629c0d1.chunk.css"
+  },
+  {
+    "revision": "464ea3ef7edd05b89ed0",
+    "url": "/snwos/static/css/4.28e9f310.chunk.css"
+  },
+  {
+    "revision": "afbbd02f225fec87288a",
+    "url": "/snwos/static/css/main.f8e3dea1.chunk.css"
+  },
+  {
+    "revision": "649f6eecdf69f8258899",
+    "url": "/snwos/static/js/2.35d3c423.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/snwos/static/js/2.35d3c423.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12b15e30905886c1867b",
+    "url": "/snwos/static/js/3.003e0ba7.chunk.js"
+  },
+  {
+    "revision": "464ea3ef7edd05b89ed0",
+    "url": "/snwos/static/js/4.e559ee1a.chunk.js"
+  },
+  {
+    "revision": "afbbd02f225fec87288a",
+    "url": "/snwos/static/js/main.e470e9ed.chunk.js"
+  },
+  {
+    "revision": "5acd0f4b9f49508d639c",
+    "url": "/snwos/static/js/runtime-main.4f503fc4.js"
+  },
+  {
+    "revision": "329eafef5f8e3ec456aeb92492eedc20",
+    "url": "/snwos/static/media/bg.329eafef.jpg"
+  }
+]);
